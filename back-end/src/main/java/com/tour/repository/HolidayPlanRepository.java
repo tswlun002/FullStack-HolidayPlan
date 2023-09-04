60dba6ec385c7c0afd53e6cc38bfab331bc2986c
Lunga Tsewu <1998tsewu@gmail.com>
2023-09-04 12:30:15 +0200

    Fix filter query by adding putting or conditions on brackets for operator precedence

--- back-end/src/main/java/com/tour/repository/HolidayPlanRepository.java
+++ back-end/src/main/java/com/tour/repository/HolidayPlanRepository.java
@@ -20,13 +20,13 @@ public interface HolidayPlanRepository extends JpaRepository<HolidayPlan, Intege
     HolidayPlan getHolidayPlan(User user, long id);
 
     @Query("select h from HolidayPlan h join fetch h.user u where " +
-            "u.username=:username and " +
+            "u.username=:username and ( " +
             "h.city in (:city) or " +
             "h.location in (:location) or " +
             "h.startDate in (:startDate) or " +
             "h.endDate in (:endDate) or " +
             "h.event in (:event) or "+
-            "h.priorityLevel in (:priorityLevel)")
+            "h.priorityLevel in (:priorityLevel))")
     Set<HolidayPlan> filterHolidayPlans(String username,List<String> city, List<String>  location, List<Date> startDate,
                                         List<Date>  endDate, List<String> event, List<Integer> priorityLevel);
 
