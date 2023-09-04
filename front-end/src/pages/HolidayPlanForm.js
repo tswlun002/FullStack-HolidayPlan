07423532da40c6de296bae53de7f73faa13961b0
Lunga Tsewu <1998tsewu@gmail.com>
2023-09-04 12:24:31 +0200

    add feature duplicate HolidayPlan

--- front-end/src/pages/HolidayPlanForm.js
+++ front-end/src/pages/HolidayPlanForm.js
@@ -1,6 +1,6 @@
 import "./HolidayPlanForm.css"
 import {FormControl,RadioGroup, FormControlLabel,Radio,FormLabel,Box,Card,CardContent,
-CardHeader,Stack}  from '@mui/material';
+CardHeader,Stack, Chip}  from '@mui/material';
 import CssTextField from '../component/CssTextField';
 import ColorButton from '../component/ColorButton';
 import { useReducer, useState} from "react"
@@ -10,12 +10,34 @@ import CustomerTypography from '../component/CustomerTypography'
 import ApploadFile from '../component/ApploadFile'
 import { ERROR_COLOR, PRIMAR_COLOR, SUCCESS_COLOR } from "../utils/Constant";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import {NavLink} from 'react-router-dom'
+import {NavLink,useLocation} from 'react-router-dom'
+import ClearIcon from '@mui/icons-material/Clear';
+
+
+export function getFileFromBase64(string64, fileName,type) {
+  const trimmedString = string64.replace('dataimage/jpegbase64', '');
+  const imageContent = atob(trimmedString);
+  const buffer = new ArrayBuffer(imageContent.length);
+  const view = new Uint8Array(buffer);
+
+  for (let n = 0; n < imageContent.length; n++) {
+    view[n] = imageContent.charCodeAt(n);
+  }
+
+  const blob = new Blob([buffer], { type });
+  return new File([blob], fileName, { lastModified: new Date().getTime(), type });
+}
+
 
 const HolidayPlanForm =()=>{
+      const location = useLocation();
+      const holidayPlanData = location.state?.holidayPlan
       const  useAxiosPrivate=UseAxiosPrivate();
-      const [imagePreview , setImagePreview] =useState([]);
-
+      const images = holidayPlanData?.images?.map((img,index)=>{
+        // `data:${img.imageType};base64, ${img.image}`
+        return getFileFromBase64(img.image,index,img.imageType);
+      });
+      const [imagePreview , setImagePreview] =useState(images?.map(file=>URL.createObjectURL(file))||[]);
        const [dateType1, setDateType1]= useState("text");
       const [dateType, setDateType]= useState("text");
 
@@ -25,10 +47,16 @@ const HolidayPlanForm =()=>{
   const[HolidayPlanData, DispatchHolidayPlanData] = useReducer((state, action)=>{
     return {...state,...action}
   },
-    {  location:"",city: "",startDate: "",endDate:"",event:"",description:"",pictureLink:"",
-       priorityLevel:"",isRequestError:false, isRequestSuccessful:false,errorMessage:""}
+    {   
+        location:holidayPlanData?.location||'',city:holidayPlanData?.city||'',
+        startDate: holidayPlanData?.startDate?.substring(0,10)||"",
+        endDate:holidayPlanData?.endDate?.substring(0,10)||"",
+        event:holidayPlanData?.event||"", description:holidayPlanData?.description||"",
+        pictureLink:"",
+       priorityLevel:holidayPlanData?.priorityLevel||"",
+       isRequestError:false, isRequestSuccessful:false,errorMessage:""}
     )
-  const[Files, setFiles]=useState([])
+  const[Files, setFiles]=useState( images||[])
   
   //Extract data from form data and store data to Data state
   const StoreData = ()=>{
@@ -36,12 +64,16 @@ const HolidayPlanForm =()=>{
          const fd = new FormData();
          fd.append("holiday",new Blob([JSON.stringify({startDate, location, endDate, event, city, description, priorityLevel},
            {'Content-Type':'application/json'})]));
-         Files.forEach(file=>fd.append("images",file));
+         Files.forEach(file=>{
+       
+          fd.append("images",file);
+        });
       AddHolidayPlan(useAxiosPrivate,fd, DispatchHolidayPlanData)
   }
 
    const isEmpty= (elementInputData)=>elementInputData.trim()==="";
-
+   
+   
    const setImages = (fileData)=>{
         const list =[];
         const list1=[];
@@ -49,6 +81,7 @@ const HolidayPlanForm =()=>{
              list.push(URL.createObjectURL(file));
              list1.push(file);
         });
+
         setImagePreview(list);
         setFiles(list1);
    };
@@ -62,8 +95,8 @@ const HolidayPlanForm =()=>{
             isEmpty(HolidayPlanData.startDate)||
             isEmpty(HolidayPlanData.endDate)||
             isEmpty(HolidayPlanData.description) || 
-            (Files.length===0 )||
-            isEmpty(HolidayPlanData.priorityLevel)
+            (Files.length===0 && imagePreview?.length===0)||
+            isEmpty(HolidayPlanData.priorityLevel?.toString())
           )
         }
 
@@ -82,6 +115,7 @@ const HolidayPlanForm =()=>{
         }
 
    }
+
 
   return (
       <Box display="flex"
@@ -106,25 +140,36 @@ const HolidayPlanForm =()=>{
 
               subheader={(HolidayPlanData.isRequestError || HolidayPlanData.isRequestSuccessful)&&HolidayPlanData.errorMessage}
               subheaderTypographyProps={{align:"start" ,color:HolidayPlanData.isRequestError?ERROR_COLOR:HolidayPlanData.isRequestSuccessful&&SUCCESS_COLOR}}
-              action={<CustomerTypography onClick={()=>{ setTimeout(()=>{
-                 DispatchHolidayPlanData(
-                   {
-                     location: "",
-                     city: "",
-                     startDate: "",
-                     endDate: "",
-                     description: "",
-                     event: "",
-                     images: [],
-                     pictureLink:"",
-                     priorityLevel: "",
-                     isRequestSuccessful:false
-                     ,isRequestError:false
+              action={
+                  <Chip
+                    onClick={()=>{ setTimeout(()=>{
+                    DispatchHolidayPlanData(
+                      {
+                        location: "",
+                        city: "",
+                        startDate: "",
+                        endDate: "",
+                        description: "",
+                        event: "",
+                        images: [],
+                        pictureLink:"",
+                        priorityLevel: "",
+                        isRequestSuccessful:false
+                        ,isRequestError:false
+      
+                      }
+                    ); setImagePreview([])
    
-                   }
-                 ); setImagePreview([])
-   
-              },2000);}}sx={{align:"end"}}>Clear form </CustomerTypography>}
+              },500);}}
+
+              icon={<ClearIcon/>}
+              label="Clear"
+              >
+            
+                Clear form 
+             
+              </Chip>
+            }
             />
                 <CardContent>
                     
@@ -194,6 +239,7 @@ const HolidayPlanForm =()=>{
 
                         <CssTextField
                             required
+                            multiline
                             id="demo-helper-text-aligned"
                             label="description"
                             variant="outlined"
@@ -208,7 +254,7 @@ const HolidayPlanForm =()=>{
                           alignItems="center">
                               {
                               imagePreview.map((image, index)=>
-                                <img style={{maxWidth:"25%", margin:"0rem 0.3rem"}}key={index} src={image} alt="Location image(s) preview"/>
+                                <img style={{maxWidth:'25%',width:100,height:50, margin:"0rem 0.3rem"}}key={index} src={image} alt="Location image(s) preview"/>
                               )
 
                               }
