0c2caeeab373204cf86f85a8e5b3f380fea26460
Lunga Tsewu <81965280+tswlun002@users.noreply.github.com>
2023-09-15 03:03:10 +0200

    Update TourApplicationTest.java
    Signed-off-by: Lunga Tsewu <81965280+tswlun002@users.noreply.github.com>

--- back-end/src/test/java/com/tour/TourApplicationTest.java
+++ back-end/src/test/java/com/tour/TourApplicationTest.java
@@ -3,11 +3,8 @@ package com.tour;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TourApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+   
 
 }
